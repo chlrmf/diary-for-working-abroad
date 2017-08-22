2b3b9c1e20528ad3e3ddec0b2c6e648f2c9140d3
My MacAIr <chlrmf@gmail.com>
2017-08-22 14:02:31 +0900

    Initial Commit

---
+++ diary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  diary
+//
+//  Created by My MacAIr on 2017. 8. 22..
+//  Copyright © 2017년 Geul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
